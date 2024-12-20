203ac1dfc3a1ce9295d30f25d4f5dca3fc66c9e2
Deedeshoe <derrickdeshoe71@gmail.com>
2024-12-20 14:09:16 -0800

    updates3

--- app/Shop/page.tsx
+++ app/Shop/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import Image from "next/image";
 import Products from "../Components/Product";
 import { HiAdjustmentsHorizontal } from "react-icons/hi2";
 import Toilet from "../../public/images/Shop/toilet.png";
